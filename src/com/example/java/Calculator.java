886ae63ca2caf14fef99a9ed4b160dd968ad4c38
Gloria <cooldude1333@yahoo.com>
2018-12-07 03:28:28 -0500

    some changes

--- src/com/example/java/Calculator.java
+++ src/com/example/java/Calculator.java
@@ -7,7 +7,7 @@ import static com.example.java.util.MathMethods.*;
 public class Calculator {
 
     public static void main(String[] args) {
-        String string11 = getInput("Enter a numeric value: ");
+        String string1 = getInput("Enter a numeric value: ");
         String s2 = getInput("Enter a numeric value: ");
         String op = getInput("Choose an operation (+ - * /):");
 
@@ -16,16 +16,16 @@ public class Calculator {
         try {
             switch (op) {
                 case "+":
-                    result = addValues(string11, s2);
+                    result = addValues(string1, s2);
                     break;
                 case "-":
-                    result = subtractValues(string11, s2);
+                    result = subtractValues(string1, s2);
                     break;
                 case "*":
-                    result = multiplyValues(string11, s2);
+                    result = multiplyValues(string1, s2);
                     break;
                 case "/":
-                    result = divideValues(string11, s2);
+                    result = divideValues(string1, s2);
                     break;
                 default:
                     System.out.println("Unrecognized operation!");
